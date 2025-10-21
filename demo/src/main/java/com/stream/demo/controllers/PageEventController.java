894d39017b1ec483e88e402bf2693058fa8e8386
ABOUCHELAILA <TON_EMAIL@exemple.com>
2025-10-21 14:28:24 +0100

    qt3:création service poducer via rest controller

---
+++ demo/src/main/java/com/stream/demo/controllers/PageEventController.java
@@ -0,0 +1,29 @@
+package com.stream.demo.controllers;
+
+import com.stream.demo.events.PageEvent;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.stream.function.StreamBridge;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+import java.util.Random;
+@RestController
+public class PageEventController {
+
+        @Autowired
+        private StreamBridge streamBridge;
+
+        @GetMapping("/publish")
+        public com.stream.demo.events.PageEvent publish(String name, String topic){
+            com.stream.demo.events.PageEvent event = new com.stream.demo.events.PageEvent(name,
+                    Math.random()>0.5?"U1":"U2",
+                    new Date(),
+                    10+new Random().nextInt(1000));
+            streamBridge.send(topic, event);
+            return event;
+        }
+
+
+
+}
---
+++ demo/src/main/java/com/stream/demo/events/PageEvent.java
@@ -0,0 +1,17 @@
+package com.stream.demo.events;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.stream.function.StreamBridge;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+import java.util.Random;
+
+
+
+
+public record PageEvent(String name, String user, Date date,long duration) {
+
+
+}
